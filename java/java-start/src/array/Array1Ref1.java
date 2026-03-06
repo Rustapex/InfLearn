b4334f1a86733cebdea629862235a9996ab8e085
YS_LABTOP\syski <sysking1009@naver.com>
2026-03-06 17:36:02 +0900

    0306 한것들

--- java/java-start/src/array/Array1Ref1.java
+++ java/java-start/src/array/Array1Ref1.java
@@ -2,10 +2,9 @@ package array;
 
 public class Array1Ref1 {
     public static void main(String[] args) {
-        int[] students;
-        students = new int[]{90,80,70,60,50};
+        int[] students = new int[]{90, 80, 70, 60, 50}; // 배열의 생성과 초기화
 
-        /*
+        /* 1번
         int[] students
         students = new int[5]
         students[0] = 90;
@@ -13,8 +12,19 @@ public class Array1Ref1 {
         students[4] = 50;
 
         */
-        for(int i=0; i<students.length; i++){
-            System.out.println("student" + (i+1) +  " = " + students[i]);
+
+        /*2번
+        * int[] students;
+        * students = new int[]{90,80,70,60,50}
+        * */
+
+        /*3번
+        * int[] students = new int[]{90,80,70,60,50}*/
+
+        /*4번
+        * int[] students = {90,80,70,60,50}*/
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("student" + (i + 1) + " = " + students[i]);
         }
 
 
---
+++ java/java-start/src/array/ArrayDi0.java
@@ -0,0 +1,28 @@
+package array;
+import java.util.Arrays;
+
+public class ArrayDi0 {
+    public static void main(String[] args) {
+        int[][] arr = new int[2][3];
+
+/*        arr[0][0] = 1;
+        arr[0][1] = 2;
+        arr[0][2] = 3;
+        arr[1][0] = 2;
+        arr[1][1] = 4;
+        arr[1][2] = 6;*/
+
+        for(int i=0; i<2; i++){
+            for(int j=0; j<3; j++){
+                arr[i][j] = (i+1) * (j+1);
+                System.out.println(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println(Arrays.deepToString(arr));
+
+
+
+    }
+}
